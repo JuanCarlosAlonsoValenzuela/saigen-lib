962d7adf576d108dfbdb14d5127248a0350092c9
Juan Carlos Alonso Valenzuela <juaaloval@gmail.com>
2021-04-13 11:23:21 +0200

    Minor update

--- src/main/kotlin/formsolver/Main.kt
+++ src/main/kotlin/formsolver/Main.kt
@@ -2,11 +2,15 @@ package formsolver
 
 import org.apache.jena.sparql.core.QuerySolutionBase
 import java.io.FileInputStream
-import java.io.IOException
-import java.util.Properties
 import edu.washington.cs.knowitall.morpha.MorphaStemmer
 import org.droidmate.saigen.Lib.Companion.getInputsForLabels
+import org.droidmate.saigen.utils.NLP
 import uk.ac.susx.informatics.Morpha
+import java.util.*
+import java.io.FileWriter
+import java.nio.file.Files
+import java.io.IOException
+import java.util.Arrays
 
 object Main {
 
@@ -16,76 +20,85 @@ object Main {
 		var kek = args
 		//val dataCrowBestOrder = arrayOf("Isbn", "Series", "Title", "Year", "Volume", "Pages", "Description", "Comment", "Webpage")
 		//val aMetro = arrayOf("Station", "Station")
-		var i = 0
-		val prova = listOf("address")
+//		val prova = listOf("author", "publisher", "serial")
+		val prova = listOf("title", "author", "isbn")
+
+//		println(testGetSynonyms("countryCode"))
+
+		val saigenResults = getInputsForLabels(prova)
+
+		var fileRoute = ".\\src\\main\\resources\\generatedTestDataSaigen\\"
+
+		for (result in saigenResults){
+			println(result.label)
+
+			var fileWriter: FileWriter? = null
+
+			try{
+				var fileName = result.label + ".csv"
+				fileWriter = FileWriter(fileRoute + fileName)
+
+				for(value in result.values) {
+					fileWriter.append(value)
+					fileWriter.append('\n')
+				}
+
+				println("$fileName Generated successfully")
+
+			} catch(e: Exception){
+				print("Error generating csv file")
+				e.printStackTrace()
+			} finally {
+				try {
+					fileWriter!!.flush()
+					fileWriter.close()
+				} catch(e: IOException) {
+					println("Flushing/closing error!")
+					println(e.printStackTrace())
+				}
+			}
 
 
-		val result = getInputsForLabels(prova)
+		}
+
+//		data class QueryResult(
+//			val label: String,
+//			val values: List<String>,
+//			val queryId: UUID
+//		)
+
+		// Write csv file with name = result.label
+		// One value per line
+
 
 		val delete = "delete"
-		//val ldapWork = arrayOf("Job", "Organisation", "Country", "Address", "City", "County", "PostCode", "Department")
-		//val ldapCont = arrayOf("FirstName", "LastName", "DisplayName", "Email", "AdditionalEmail", "WorkPhone", "HomePhone", "Fax", "Mobile")
-		//val ldapContWork = arrayOf("name", "surname", "DisplayName", "Email", "AdditionalEmail", "WorkPhone", "HomePhone", "Fax", "Mobile", "Job", "Organisation", "Country", "Address", "City", "County", "PostCode", "Department")
-//		var myjlibrary = arrayOf("Isbn", "Title", "Publisher", "Author", "Published", "Description", "Language", "Pages")
-//		//val flight = arrayOf("Airport", "Airline")
-//		//myjlibrary = arrayOf("Isbn", "Book", "Author", "Genre")
-//		myjlibrary = arrayOf("Street", "City")
-//		myjlibrary[0] = "State"
-//		val sogliaAssociazioni = 50
-//		val usoSoglia = false
-//		val euristica = 2
-//		val prop = Properties()
-//		var input: FileInputStream? = null
-//		try {
-//			input = FileInputStream("config.properties")
-//			prop.load(input)
-//			if (prop.getProperty("endpoint") != null) {
-//				FormSolver.endpoint = prop.getProperty("endpoint")
-//			}
-//			if (prop.getProperty("labels") != null) {
-//				internalArgs = prop.getProperty("labels").split(";".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
-//
-//			}
-//		} catch (ex: IOException) {
-//			ex.printStackTrace()
-//			System.exit(-1)
-//		}
-//
-//		if (internalArgs.isEmpty()) {
-//			println("Missing inputs")
-//			return
-//		}
-//		var etichette = internalArgs
-//		while (etichette.isNotEmpty()) {
-//			val risolutore = FormSolver(etichette, usoSoglia, sogliaAssociazioni, euristica)
-//			val etichetteTagliate = risolutore.runProcess(1)	// TODO: CHANGE VALUE!!!
-//			val delete = "delete"
-//
-////			etichette = etichetteTagliate.toTypedArray()
-//		}
-//		i = 0
-//		while (i < CommonData.outputQueries.size) {
-//			val query2 = CommonData.outputQueries[i]
-//			println("*********************************")
-//			println("*********************************")
-//			println("*********************************")
-//			println(query2)
-//			println("i: $i")
-//			++i
-//		}
-//
-//		val aaa = edu.washington.cs.knowitall.morpha.MorphaStemmer.stem("cities dogs and cats need to stayed together for the stars. addresses are ten twenty fives. geese are cool. lets go to parties")
-//
-//
-//		val bbb = uk.ac.susx.informatics.Morpha.noun
-//
-//		println("++++++++++++++++++++HERE IS THE aaa: $aaa")
-//
-//
-//		val pairy = Pair("adogo", "aCATDO")
-//		println(pairy)
-		//println("http://dbpedia.org/resource/Rock_music".replaceBeforeLast("/", ""))
-		//println("http://dbpedia.org/resource/Rock_music".substringAfterLast("/"))
-		//println("http://dbpedia.org/resource/Rock_music".substringAfterLast("/").replace("_"," "))
+	}
+
+	private val cache = mutableMapOf<String, List<String>>()
+
+	fun testGetSynonyms(word: String): List<String> {
+
+		if (!cache.containsKey(word)) {
+			try {
+				val words = WordNet.getSynonyms(word)
+					.map { it.replace("_", " ") }
+					.map {
+						it.map { c ->
+							if (c in 'A'..'Z')
+								" $c"
+							else
+								"$c"
+						}.joinToString("")
+					}
+//					.filterNot { it.contains(" ") }
+					.distinct()
+				words.forEach { cache[it] = words }
+			} catch (e: Exception) {
+				cache[word] = emptyList()
+				NLP.log.error("Unable to get synonym for $word", e)
+			}
+		}
+
+		return cache[word].orEmpty()
 	}
 }
